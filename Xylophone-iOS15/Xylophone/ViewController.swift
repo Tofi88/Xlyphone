9096c31bcd5fe7a6fd29af88a08a35b5f20f989b
tofii <103397108+Tofi88@users.noreply.github.com>
2022-04-28 03:07:28 +0200

    Add files via upload

---
+++ Xylophone-iOS15/Xylophone/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Mostafa ElBigarmii  on 28/04/2022.
+//  Copyright © 2022 The App BlackTofii. All rights reserved.
+//
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
+
+
